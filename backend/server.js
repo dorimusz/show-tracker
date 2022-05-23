af394baebc68914d5591a91b3b3f955df3a4c794
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-05-23 10:38:09 +0200

    cors options

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,13 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
-app.use(cors());;
-app.use(express.json());;
+
+const corsOptions = {
+    origin: process.env.HOST,
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));;
+app.use(express.json());
 
 
 app.get('/', (req, res) => {
