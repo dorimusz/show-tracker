a7e13808cd3e81defd4634511527a770a2794176
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-05-24 09:42:08 +0200

    placing error handler

--- backend/server.js
+++ backend/server.js
@@ -29,15 +29,11 @@ const myLoggerMiddleware = (req, res, next) => {
 
 const myAuthMiddleware = (req, res, next) => {
     console.log("Authenticating...");
-    let a;
-    console.log(a.b.c);
-
-    // const userID = 5;
-    const userID = null;
+    const userID = 5;
+    // const userID = null;
     //req.userID = userID; //ráaggatom a request objectre, mert ez utazik szépen tovább badum tss. - not best practice, nehogy felülírja a default API-t
     res.locals.userID = userID //ez a konvenció, ezt szoktuk követni, hogy erre pakoljuk rá, hogy ne rontsunk el esszenciális dolgokat
     next();
-
 };
 
 const myBusinessLogicMiddleware = (req, res) => {
@@ -51,11 +47,11 @@ app.use(myLoggerMiddleware);
 app.use(myAuthMiddleware);
 app.use(myBusinessLogicMiddleware);
 
-app.use(errorHandlerMiddleware) //mivel 4 bemeneti paraméter van, tudni fogja, hogy ez error handler - de az err az első!!! és utolsóként regisztráljuk, app.get és route se legyen már utána
-
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // })
+
+app.use(errorHandlerMiddleware) //mivel 4 bemeneti paraméter van, tudni fogja, hogy ez error handler - de az err az első!!! és utolsóként regisztráljuk, app.get és route se legyen már utána
 
 app.listen(port, () => {
     console.log(`To do app is listening on port ${port}`)
