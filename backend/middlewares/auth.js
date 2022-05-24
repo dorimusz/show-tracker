32499807bfefd64c9684901142dbc6fb5b6dd3de
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-05-24 10:35:40 +0200

    middleware connected to a specific endpoint

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,10 +1,20 @@
 const auth = (req, res, next) => {
     console.log("Authenticating...");
+    const userID = req.header('authorization'); //ide jön majd, hgy jwt.vel verifyoljuk
+    res.locals.userID = userID;
+    next();
+};
+
+module.exports = auth;
+
+
+/*
+const auth = (req, res, next) => {
+    console.log("Authenticating...");
     const userID = 5;
     // const userID = null;
     //req.userID = userID; //ráaggatom a request objectre, mert ez utazik szépen tovább badum tss. - not best practice, nehogy felülírja a default API-t
     res.locals.userID = userID //ez a konvenció, ezt szoktuk követni, hogy erre pakoljuk rá, hogy ne rontsunk el esszenciális dolgokat
     next();
 };
-
-module.exports = auth;
+*/
--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,21 @@ app.use(express.json());
 
 //ezek mind lefutottak, minden hívásnál
 app.use(logger);
-app.use(auth);
+// app.use(auth); //de ezt nem akarom minden endpointál meghívni, ezért elehlyezhetem másként, a (req, res) elé
 
-app.get('/api/logic1', (req, res) => {
-    console.log('logic1');
-    res.send('Hello world! - 1');
+app.get('/api/public', (req, res) => {
+    console.log('public');
+    res.send('Hello world! - public');
 })
 
-app.get('/api/logic2', (req, res) => {
-    console.log('logic2');
-    res.send('Hello world! - 2');
+app.get('/api/private', auth, (req, res) => {
+    console.log('private');
+    res.send(`Hello world! - private ${res.locals.userID}`);
+})
+
+app.get('/api/prublic', auth, (req, res) => {
+    if (!res.locals.userID) return res.send("Hello world! - pRublic");
+    res.send(`Hello world! - pRublic ${res.locals.userID}`);
 })
 
 app.use(errorHandler) //mivel 4 bemeneti paraméter van, tudni fogja, hogy ez error handler - de az err az első!!! és utolsóként regisztráljuk, app.get és route se legyen már utána
