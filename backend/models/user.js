10e858101e3723e66f726b0339ea56ca95497503
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-05-25 09:59:48 +0200

    schemas and model for database

---
+++ backend/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+//subdocuments, no need to make model out of them
+const todoSchema = new mongoose.Schema({
+    title: { type: String, required: true }, //empty string is not accepted
+    description: { type: String, required: true }, //empty string is enough
+    isDone: { type: Boolean, default: false },
+});
+
+const dashboardSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    todos: [todoSchema],
+});
+
+//uses the subdocuments
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true }, //empty string is not accepted
+    email: { type: String, unique: true, required: true }, //empty string is not accepted + validation
+    password: { type: String, required: true }, //empty string is not accepted + validation
+    dashboards: [dashboardSchema], //empty list as default
+});
+
+const userModel = mongoose.model("user", userSchema);
+module.exports = userModel;
