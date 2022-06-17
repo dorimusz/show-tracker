d837ecddaf15cfa618844142d37336aa552fe2be
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-06-17 10:01:04 +0200

    some comments added

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -5,7 +5,7 @@ import { useAuth } from "../providers/auth";
 
 //speciális props-t kap
 const Protected = ({ children }) => {
-    const { token } = useAuth();
+    const { token } = useAuth(); //nem a storageből szedjük, mert nem reaktív, csak így tud rá reagálni
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -16,6 +16,7 @@ const Protected = ({ children }) => {
 
     return (
         <>
+            {/* <React.Fragment>{children}</React.Fragment> */}
             {children}
         </>
     )
