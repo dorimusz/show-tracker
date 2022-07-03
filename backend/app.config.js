77e304dc45666f68eccdf31f4c1f68c168a71679
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-07-03 11:55:54 +0200

    fixed .env names

--- backend/app.config.js
+++ backend/app.config.js
@@ -2,8 +2,8 @@ const config = {
     auth:
     {
         google: {
-            client_id: process.env.CLIENT_ID_GOOGLE || '3423125049963-vnhlm59vvirdjsquu0efhqvq5u91orks.apps.googleusercontent.com',
-            client_secret: process.env.CLIENT_SECRET_GOOGLE || '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',
+            client_id: process.env.GOOGLE_CLIENT_ID || '3423125049963-vnhlm59vvirdjsquu0efhqvq5u91orks.apps.googleusercontent.com',
+            client_secret: process.env.GOOGLE_CLIENT_SECRET || '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',
             redirect_uri: "http://localhost:3000/callback",
             token_endpoint: "https://oauth2.googleapis.com/token",
             grant_type: "authorization_code",
@@ -20,8 +20,8 @@ const config = {
             user_id: null,
         },
         github: {
-            client_id: process.env.CLIENT_ID_GITHUB,
-            client_secret: process.env.CLIENT_SECRET_GITHUB,
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
             redirect_uri: "http://localhost:3000/callback/github",
             token_endpoint: "https://github.com/login/oauth/access_token",
             grant_type: "authorization_code",
