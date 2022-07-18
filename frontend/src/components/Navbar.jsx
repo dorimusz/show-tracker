f27060503e01e0158dae12841310dbdcd188b412
dorimusz <78429829+dorimusz@users.noreply.github.com>
2022-07-18 21:20:18 +0200

    navbar redesign

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../providers/auth';
+import '../styles/Navbar.css'
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -12,30 +13,46 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className='navbar' style={{ backgroundColor: "gray", position: "absolute", top: "0", width: "100%", display: "flex", justifyContent: "space-between" }}>
-                <div>
-                    <button onClick={() => nav('/')}>Home</button>
-                    <button onClick={() => nav('/search')} > Search</button>
+            {token ?
+                <nav className='navbarUser'>
+                    <div className='navWatchlist'>
+                        <button className='watchlistButton' onClick={() => nav('/watchlist')} > My watchlist</button>
+                    </div>
+                    <div className='navControls'>
+                        <section className='trackSeries'>
+                            <div className='headingHolder'>
+                                <p className='heading'>Track TV shows</p>
+                            </div>
+                            <button onClick={() => nav('/search')} > Search</button>
+                            <button onClick={() => nav('/request')} > Request a show</button>
+                            <button onClick={() => nav('/myshows/manage')} > Manage my shows</button>
+                        </section>
+                        <section className='profileSettings'>
+                            <div className='headingHolder'>
+                                <p className='heading'>Settings</p>
+                            </div>
+                            <button onClick={() => nav('/profile')} > Profile</button>
+                            <button className='logout' onClick={logout}>Log out</button>
+                        </section>
+                    </div>
+                </nav >
 
-                    {token ? <button onClick={() => nav('/watchlist')} > Watchlist</button> : null}
+                :
 
-                    {token ? <button onClick={() => nav('/myshows/manage')} > Manage shows</button> : null}
+                <nav className='navbar'>
+                    <div>
+                        <button onClick={() => nav('/')}>Home</button>
+                        <button onClick={() => nav('/search')} > Search</button>
 
-                    {token ? <button onClick={() => nav('/request')} > Request show</button> : null}
+                    </div>
+                    <div>
+                        <button className='login' onClick={() => auth('google')}>Google login</button>
+                        <button className='login' onClick={() => auth('oid')}>Login</button>
 
-                    {token ? <Link to="/profile" > Profile</Link> : null}
-                </div>
-                <div>
-                    {token ?
-                        <button onClick={logout}>Log out</button>
-                        :
-                        <>
-                            <button onClick={() => auth('google')}>Google login</button>
-                            <button onClick={() => auth('oid')}>Login</button>
-                        </>
-                    }
-                </div>
-            </nav >
+
+                    </div>
+                </nav >
+            }
         </>
     )
 }
